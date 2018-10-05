e98c5fba07fb744c060e8e20d566591f428714d1
foxniver <32783730+foxniver@users.noreply.github.com>
2018-10-04 19:52:25 -0500

    Adding javadoc comments to UserRole and UserLesson to explain the many-to-many relationship set-up.

--- src/main/java/com/danceUpByStas/entity/UserLesson.java
+++ src/main/java/com/danceUpByStas/entity/UserLesson.java
@@ -5,6 +5,13 @@ import lombok.Data;
 import javax.persistence.*;
 import java.io.Serializable;
 
+/**
+ * This entity is created to reflect the many-to-many relationship between User and Lesson.
+ * Even though, there are only 2 columns in this entity, the set-up is designed to accomodate
+ * the potential expansion of the entity by adding some other fields later on. Some examples
+ * may include a primary key or a timestamp to record when the association between the user and
+ * the lesson took place.
+ */
 @Entity(name = "UserLesson")
 @Table(name = "User_Lesson")
 @Data
--- src/main/java/com/danceUpByStas/entity/UserRole.java
+++ src/main/java/com/danceUpByStas/entity/UserRole.java
@@ -6,6 +6,12 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import java.io.Serializable;
 
+/**
+ * This entity was created with the purpose of accomodation for the future expansion.
+ * Some extra fields like the primary key or timestamp may be added to provide
+ * more data about the relationship between User and Role entities.
+ */
+
 @Entity(name = "UserRole")
 @Table(name = "User_Role")
 public class UserRole implements Serializable {
