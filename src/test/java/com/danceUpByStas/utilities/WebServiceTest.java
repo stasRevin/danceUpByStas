ed22ffb114a6c49a6358fdef88551b45960be55d
foxniver <32783730+foxniver@users.noreply.github.com>
2018-10-19 21:33:09 -0500

    Adding test for zip code radius web service.

---
+++ src/test/java/com/danceUpByStas/utilities/WebServiceTest.java
@@ -0,0 +1,39 @@
+package com.danceUpByStas.utilities;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class WebServiceTest {
+
+    Logger logger = LogManager.getLogger(this.getClass());
+
+    @Test
+    void getNearbyZipCodesSuccess() {
+        // https://www.zipwise.com/webservices/?
+        Client client = ClientBuilder.newClient();
+        String uri = "https://www.zipwise.com/webservices/radius.php?key=ehwjedqpjnm7xdsc&zip=90210&radius=2&format=json";
+
+        Map<String, String> jsonObject = client.target(uri).request(MediaType.APPLICATION_JSON).get(LinkedHashMap.class);
+
+        List list = (List) new ArrayList(jsonObject.values()).get(0);
+
+        LinkedHashMap<String, String> firstLocation = (LinkedHashMap<String, String>)list.get(0);
+        String zipCode = firstLocation.get("zip");
+
+        client.close();
+
+        assertEquals("90210", zipCode);
+
+
+    }
+
+}
